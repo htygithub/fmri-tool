aaa45b3edac18c3dfec987daacad4478db20fa0a
htygithub <tyhuang@mail.ntust.edu.tw>
2016-11-02 16:58:07 +0800

    update

--- fsl_stat.py
+++ fsl_stat.py
@@ -4,6 +4,7 @@ import os
 from glob import glob,iglob
 from os.path import join
 from argparse import ArgumentParser
+import datetime
 #Path_current=sys.argv[0]
 #N_iter=sys.argv[1]
 #Name_Rmapfile=sys.argv[2]
@@ -13,7 +14,7 @@ parser = ArgumentParser()
 parser.add_argument("-c", dest="control_dir", help="Path of control data",action='store',default='ctrl')
 parser.add_argument("-e", dest="exp_dir", help="Path of experiement data",action='store',default='exp')
 parser.add_argument("-n", dest="N_iter", help="Number of iteration",action='store',default=100,type=int)
-parser.add_argument("-f", dest="Name_Rmapfile", help="File name of Rmap",action='store',default='Rmap_beswarrest.nii')
+parser.add_argument("-f", dest="Name_Rmapfile", help="File name of Rmap, Default: Rmap_beswarrest.nii",action='store',default='Rmap_beswarrest.nii')
 parser.add_argument("-a", dest="allnii", help="Get all nii files, disregarding -f",action='store_true')
 args = parser.parse_args()
 
@@ -22,6 +23,7 @@ print(args.N_iter)
 Path_current = os.getcwd()
 N_iter=args.N_iter
 #Name_Rmapfile='Rmap_beswarrest.nii'
+result_dir = join(Path_current,datetime.datetime.now().strftime("stat%m%d%H%M%S"))
 
 if args.allnii:
     # 分別讀取 Path_current下面的cont與exp資料夾內的MARS結果資料夾，直接從各資料夾中讀取名稱為 Name_Rmapfile(Rmap_beswarrest.nii)的檔案
@@ -43,10 +45,10 @@ for ii in range(len(list_experim)):print('%d:%s' % (ii,list_experim[ii]))
 if len(list_control) > 0:
     str_control = ' '.join(list_control)
     # 把所有control受試者的Rmap合併成一個四維的Rmaps
-    str_OSins='fslmerge -t '+ Path_current + '/contRmaps '+ str_control
+    str_OSins='fslmerge -t %s %s ' % (join(result_dir,'contRmaps'),str_control)
     os.system(str_OSins)
     # 做出control的One sample t-test結果
-    str_OSins='randomise -i %s/contRmaps -o %s/contOnettst -1  -n %d --T2'%(Path_current ,Path_current ,N_iter)
+    str_OSins='randomise -i %s/contRmaps -o %s/contOnettst -1  -n %d --T2' % (result_dir ,result_dir ,N_iter)
     os.system(str_OSins)
 
 
@@ -54,10 +56,11 @@ if len(list_control) > 0:
 if len(list_experim) > 0:
     str_experim = ' '.join(list_experim)
     # 把所有Expriment受試者的Rmap合併成一個四維的Rmaps
-    str_OSins='fslmerge -t '+ Path_current + '/expRmaps '+ str_experim
+    # str_OSins='fslmerge -t '+ Path_current + '/expRmaps '+ str_experim
+    str_OSins='fslmerge -t %s %s ' % (join(result_dir,'expRmaps'),str_experim)
     os.system(str_OSins)
     # 做出Expriment的One sample t-test結果
-    str_OSins='randomise -i %s/expRmaps  -o %s/expOnettst -1  -n %d --T2'%(Path_current ,Path_current ,N_iter)
+    str_OSins='randomise -i %s/expRmaps  -o %s/expOnettst -1  -n %d --T2'%(result_dir ,result_dir ,N_iter)
     os.system(str_OSins)
 
 
@@ -68,15 +71,15 @@ if len(list_experim) > 0:
 
 if len(list_experim) > 0:
     # 把兩個群組的Rmaps合併成一個，並且由design.mat決定群組
-    str_OSins='fslmerge -t allRmaps '+ Path_current + '/contRmaps ' + Path_current + '/expRmaps '
+    str_OSins='fslmerge -t allRmaps '+ result_dir + '/contRmaps ' + result_dir + '/expRmaps '
     os.system(str_OSins)
 
     # 若兩組資料只是單純的兩群不同受試者比較Two-Sample Unpaired T-test，而非Two-Sample Paired T-test (Paired Two-Group Difference)
     # 可以用 design_ttest2 檔案名稱 群組A數目 群組B數目 來建立design.mat以及design.con
     # design.mat是GLM用來分辨不同群組的矩陣。design.con是不同群組之間的contrast(譬如說A >B 或A<B)
-    str_OSins='design_ttest2 %s/design %d %d'%(Path_current , len(list_control ),len(list_experim ))
+    str_OSins='design_ttest2 %s/design %d %d' % (result_dir , len(list_control),len(list_experim))
     os.system(str_OSins)
 
     # 執行兩群組之間的Two sample t-test
-    str_OSins='randomise -i %s/allRmaps -o %s/resultTwottst -d design.mat -t design.con --T2 -n %d'%(Path_current,Path_current  ,N_iter)
+    str_OSins='randomise -i %s/allRmaps -o %s/twosample -d design.mat -t design.con --T2 -n %d' % (result_dir, result_dir, N_iter)
     os.system(str_OSins)
