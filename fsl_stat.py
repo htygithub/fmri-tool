4dd88e9a58625ff51d89a0fd4c81b1c339b01676
htygithub <tyhuang@mail.ntust.edu.tw>
2016-11-02 17:35:43 +0800

    update

--- fsl_stat.py
+++ fsl_stat.py
@@ -47,14 +47,24 @@ for ii in range(len(list_control)):print('%d:%s' % (ii,list_control[ii]))
 print('Exp datafiles:')
 for ii in range(len(list_experim)):print('%d:%s' % (ii,list_experim[ii]))
 
+ctrl_rmap_ff = join(result_dir,'ctrlRmaps')
+exp_rmap_ff = join(result_dir,'expRmaps')
+all_rmap_ff = join(result_dir,'allRmaps')
+ctrl_1ample_ff = join(result_dir,'ctrl_1sample')
+exp_1sample_ff = join(result_dir,'exp_1sample')
+twosample_ff = join(result_dir,'twosample')
+design_ff = join(result_dir,'design')
+design_mat_ff = join(result_dir,'design.mat')
+design_con_ff = join(result_dir,'design.con')
+
 # 列出control內所有的影像路徑
 if len(list_control) > 0:
     str_control = ' '.join(list_control)
     # 把所有control受試者的Rmap合併成一個四維的Rmaps
-    str_OSins='fslmerge -t %s %s ' % (join(result_dir,'contRmaps'),str_control)
+    str_OSins='fslmerge -t %s %s ' % (ctrl_rmap_ff,str_control)
     os.system(str_OSins)
     # 做出control的One sample t-test結果
-    str_OSins='randomise -i %s/contRmaps -o %s/contOnettst -1  -n %d --T2' % (result_dir ,result_dir ,N_iter)
+    str_OSins='randomise -i %s -o %s -1  -n %d --T2' % (ctrl_rmap_ff, ctrl_1sample_ff, N_iter)
     os.system(str_OSins)
 
 
@@ -63,29 +73,24 @@ if len(list_experim) > 0:
     str_experim = ' '.join(list_experim)
     # 把所有Expriment受試者的Rmap合併成一個四維的Rmaps
     # str_OSins='fslmerge -t '+ Path_current + '/expRmaps '+ str_experim
-    str_OSins='fslmerge -t %s %s ' % (join(result_dir,'expRmaps'),str_experim)
+    str_OSins='fslmerge -t %s %s ' % (exp_rmap_ff,str_experim)
     os.system(str_OSins)
     # 做出Expriment的One sample t-test結果
-    str_OSins='randomise -i %s/expRmaps  -o %s/expOnettst -1  -n %d --T2'%(result_dir ,result_dir ,N_iter)
+    str_OSins='randomise -i %s -o %s -1  -n %d --T2' % (exp_rmap_ff, exp_1sample_ff, N_iter)
     os.system(str_OSins)
-
-
-
-
-
 
 
 if len(list_experim) > 0:
     # 把兩個群組的Rmaps合併成一個，並且由design.mat決定群組
-    str_OSins='fslmerge -t '+ result_dir + '/allRmaps '+ result_dir + '/contRmaps ' + result_dir + '/expRmaps '
+    str_OSins='fslmerge -t %s %s %s' % (all_rmap_ff,ctrl_rmap_ff,exp_rmap_ff)
     os.system(str_OSins)
 
     # 若兩組資料只是單純的兩群不同受試者比較Two-Sample Unpaired T-test，而非Two-Sample Paired T-test (Paired Two-Group Difference)
     # 可以用 design_ttest2 檔案名稱 群組A數目 群組B數目 來建立design.mat以及design.con
     # design.mat是GLM用來分辨不同群組的矩陣。design.con是不同群組之間的contrast(譬如說A >B 或A<B)
-    str_OSins='design_ttest2 %s/design %d %d' % (result_dir , len(list_control),len(list_experim))
+    str_OSins='design_ttest2 %s %d %d' % (design_ff, len(list_control),len(list_experim))
     os.system(str_OSins)
 
     # 執行兩群組之間的Two sample t-test
-    str_OSins='randomise -i %s/allRmaps -o %s/twosample -d design.mat -t design.con --T2 -n %d' % (result_dir, result_dir, N_iter)
+    str_OSins='randomise -i %s -o %s -d %s -t %s --T2 -n %d' % (all_rmap_ff, twosample_ff,design_mat_ff, design_con_ff, N_iter)
     os.system(str_OSins)
