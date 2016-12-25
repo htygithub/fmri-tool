9dccd7a46437966623876448cba018d9a054b072
Teng-Yi Huang <tyhuang@mail.ntust.edu.tw>
2016-12-26 06:51:03 +0800

    update

--- fsl_stat4Dmp.py
+++ fsl_stat4Dmp.py
@@ -142,8 +142,10 @@ args = parser.parse_args()
 Path_current = os.getcwd()
 N_iter=args.N_iter
 #Name_Rmapfile='Rmap_beswarrest.nii'
-result_dir = join(Path_current,datetime.datetime.now().strftime("stat%m%d_%H%M%S_") + \
-                  args.Name_Rmapfile.split('.')[0] + 'N%d' % args.N_iter)
+result_dir = join(Path_current,datetime.datetime.now().strftime("S%m%d_%H%M%S_") + \
+                  args.Name_Rmapfile.split('.')[0] + '_N%d' % args.N_iter)
+if args.tfce:
+    result_dir += '_tfce'
 safe_mkdir(result_dir)
 if args.allnii:
     # 分別讀取 Path_current下面的cont與exp資料夾內的MARS結果資料夾，直接從各資料夾中讀取名稱為 Name_Rmapfile(Rmap_beswarrest.nii)的檔案
