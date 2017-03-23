76875670dd40f4a3c87eb711e8f00461f3fd7e9c
htygithub <tyhuang@mail.ntust.edu.tw>
2017-03-23 17:24:46 +0800

    update

--- collect_abide.py
+++ collect_abide.py
@@ -6,9 +6,10 @@ import shutil
 #matfile = loadmat('abide_mars2_0050002_3726')
 v1 = pd.read_csv(r'Phenotypic_V1_0b_preprocessed1.csv')
 
-v2=v1[(v1.qc_rater_1 != 'fail') & (v1.qc_anat_rater_2 != 'fail') & \
-      (v1.qc_func_rater_2 != 'fail') & (v1.qc_func_rater_3 != 'fail')]
+#v2=v1[(v1.qc_rater_1 != 'fail') & (v1.qc_anat_rater_2 != 'fail') & \
+#      (v1.qc_func_rater_2 != 'fail') & (v1.qc_func_rater_3 != 'fail')]
 #data=v2.as_matrix()
+v2 = v1[v1.SRS_RAW_TOTAL>0]
 files = glob.glob(r'*')
 SUB_ID = v2.SUB_ID
 DX_GROUP = v2.DX_GROUP
